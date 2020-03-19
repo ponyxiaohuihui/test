ef1bfbcaa914da49cdfa4b0f92590298ff218193
pony <pony@finereport.com>
2020-03-19 14:40:22 +0800

    volatile, sync visible ,cleaner reference test

--- src/main/java/com/winphysoft/test/ref/PrefTest.java
+++ src/main/java/com/winphysoft/test/ref/PrefTest.java
@@ -1,5 +1,8 @@
 package com.winphysoft.test.ref;
 
+import sun.misc.Cleaner;
+import sun.misc.SharedSecrets;
+
 import java.lang.ref.PhantomReference;
 import java.lang.ref.Reference;
 import java.lang.ref.ReferenceQueue;
@@ -11,12 +14,21 @@ import java.lang.reflect.Field;
  * Created by pony on 2020/3/18
  */
 public class PrefTest {
+    private Cleaner cleaner;
+    private int id;
+
+
+    public PrefTest(int id) {
+        this.id = id;
+        this.cleaner = Cleaner.create(this, new PrefCleaner(id));
+    }
+
     public static void main(String[] args) throws Exception {
-       mypref();
+        cleanerPref();
 //        copyRef();
     }
 
-    private static void copyRef() throws Exception{
+    private static void copyRef() throws Exception {
         final ReferenceQueue referenceQueue = new ReferenceQueue();
         new Thread() {
             public void run() {
@@ -28,7 +40,7 @@ public class PrefTest {
                                     .getDeclaredField("referent");
                             rereferent.setAccessible(true);
                             Object result = rereferent.get(obj);
-                            System.out.println("gc will collect：" +  result);
+                            System.out.println("gc will collect：" + result);
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -36,8 +48,9 @@ public class PrefTest {
                 }
             }
         }.start();
-        while (true){
-            PrefTest abc = new PrefTest();
+        int i = 0;
+        while (true) {
+            PrefTest abc = new PrefTest(i++);
             System.out.println("new " + abc.hashCode());
             PhantomReference abcWeakRef = new PhantomReference(abc,
                     referenceQueue);
@@ -49,15 +62,15 @@ public class PrefTest {
 
     }
 
-    public static void mypref() throws InterruptedException {
+    public static void myPref() throws InterruptedException {
         final ReferenceQueue queue = new ReferenceQueue();
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
-                while (true){
+                while (true) {
                     try {
-                        Object ob  = queue.remove();
-                        if (ob != null){
+                        Object ob = queue.remove();
+                        if (ob != null) {
                             Field rereferent = Reference.class
                                     .getDeclaredField("referent");
                             rereferent.setAccessible(true);
@@ -70,13 +83,46 @@ public class PrefTest {
                 }
             }
         }.start();
-        while (true){
+        int i = 0;
+        while (true) {
             Thread.sleep(10000);
-            PrefTest ob = new PrefTest();
+            PrefTest ob = new PrefTest(i++);
             PhantomReference reference = new PhantomReference(ob, queue);
             System.out.println("new " + ob);
             ob = null;
             System.gc();
+        }
+    }
+
+    public static void cleanerPref() throws InterruptedException {
+        int i = 0;
+        while (true) {
+            Thread.sleep(1000);
+            PrefTest ob = new PrefTest(i++);
+            System.out.println("new " + ob);
+//            ob = null;
+            System.gc();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "PrefTest{" +
+                "id=" + id +
+                '}';
+    }
+
+    protected static class PrefCleaner implements Runnable {
+
+        private int id;
+
+        public PrefCleaner(int id) {
+            this.id = id;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("release " + id);
         }
     }
 
