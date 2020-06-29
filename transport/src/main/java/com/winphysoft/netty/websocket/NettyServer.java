e523e24cab9d7def00f1c9d0e369e97e82a19377
pony <pony@fanruan.com>
2020-06-29 22:45:31 +0800

    wsstart

---
+++ transport/src/main/java/com/winphysoft/netty/websocket/NettyServer.java
@@ -0,0 +1,26 @@
+package com.winphysoft.netty.websocket;
+
+import com.winphysoft.netty.Server;
+import com.winphysoft.netty.heartbeat.HeartBeatHandler;
+import io.netty.handler.codec.http.HttpClientCodec;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.stream.ChunkedWriteHandler;
+import io.netty.handler.timeout.IdleStateHandler;
+
+import java.util.concurrent.TimeUnit;
+
+public class NettyServer extends Server {
+    public static void main(String[] args) throws Exception{
+        startServer(new HttpClientCodec(),
+                new ChunkedWriteHandler(),
+                /**
+                 * http 聚合
+                 */
+                new HttpObjectAggregator(8192),
+                /**
+                 * ws://localhost:port/wstest
+                 */
+                new WebSocketServerProtocolHandler("/wstest"));
+    }
+}
